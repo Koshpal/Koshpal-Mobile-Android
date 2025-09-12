fd2ba6160933eb9d4fa0de6a256bb84bef6ec347
ChaitanySKakde <chaitanyskakde@ChaitanySKakdes-MacBook-Pro.local>
2025-09-12 23:05:53 +0530

    Secondcommit

--- app/src/main/java/com/koshpal_android/koshpalapp/Application.kt
+++ app/src/main/java/com/koshpal_android/koshpalapp/Application.kt
@@ -1,9 +1,13 @@
 package com.koshpal_android.koshpalapp
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
+        
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)
     }
 }
